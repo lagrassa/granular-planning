74ce946451df9d3bcdda8760b823ca29db0eed69
Alex LaGrassa <lagrassa@mit.edu>
2020-11-19 15:33:22 -0500

    specs from alex

---
+++ envs/block_env.py
@@ -0,0 +1,25 @@
+import pybullet as p
+class Simulator:
+    def __init__(self, gui=False):
+        pass
+    def set_state(self, state):
+        """
+        Given planner state, sets the simulator state to reflect that by teleporting
+        :param state:
+        :return:
+        """
+        pass
+    def apply_action(self, action):
+        """
+        Applies action to environment and then simulates it
+        :param action:
+        :return:
+        """
+        pass
+    def get_state(self, state):
+        """
+        Returns the current state
+        :param state:
+        :return:
+        """
+        pass
---
+++ forward/transition_models.py
@@ -0,0 +1,37 @@
+def is_free_space_motion(state, action):
+    """
+
+    :param state:  beginning state
+    :param action: action
+    :return: whether the action occurs in free space. Mostly just uses collision_fn from state
+    """
+    pass
+
+def transition_model(state, action, simulator):
+    """
+    Given current state and action return next state
+    :param state:
+    :param action:
+    :param simulator instance
+    :return:
+    """
+    pass
+
+def _free_space_transition_model(state, action):
+    """
+    Determines next state, requires that this is a free space motion
+    :param state:
+    :param action:
+    :return:
+    """
+    pass
+
+def _with_blocks_transition_model(state, action, simulator):
+    """
+    Using a simulator, returns the next state
+    :param state:
+    :param action:
+    :param simulator:
+    :return:
+    """
+    pass
