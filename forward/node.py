53a5a3df4ed561323f1cd540a979b3242a86a269
juanluivn <45583385+juanluivn@users.noreply.github.com>
2020-11-21 17:25:52 -0500

    Update node.py

--- forward/node.py
+++ forward/node.py
@@ -1,13 +1,13 @@
 # from .transition_models import get_successors
 
 class Node:
-    def __init__(self, robotState=None, envState=None):
+    def __init__(self, g=1e10, robotState=None, envState=None):
         """
         :param robotState: if provided, should be a 1D numpy array of length 4, representing (t, x, y, theta).
         :param envState: if provided, should be a Nx2 numpy array of [(x, y) for block1, (x, y) for block 2, ...]
         """
         self.h = 0.0
-        self.g = 1e10
+        self.g = g
         self.robotState = robotState
         self.envState = envState
         self.parent_id = None
