cf371f916f0b7f13e62b267dfc09e4a44d02ba04
juanluivn <45583385+juanluivn@users.noreply.github.com>
2020-11-21 17:24:30 -0500

    Update astar.py

---
+++ planning/astar.py
@@ -0,0 +1,33 @@
+
+def ComputePath(OPEN, s_goal):
+	path = []
+	CLOSED = []
+	while len(OPEN) > 0:
+		# Extract S with smallest f from OPEN
+		if(S.isGoal()):	# Create function to compare two instances of class Node
+			break
+		CLOSED.append(S)
+		successors = GenerateSuccessors(S)
+		for s_prime in successors:
+			if s_prime not in CLOSED:
+#				cost =
+				if s_prime.g > (S.g + cost):
+					s_prime.g = S.g + cost
+					OPEN.append(s_prime)
+	return path
+
+def GenerateSuccessors(S):
+	s_primes = []
+	# Generate all successors of Node S
+	return s_primes
+
+def A_star():
+	OPEN = []
+	solution = []
+	g = 0
+	s_start = Node(g)
+	s_goal = Node()
+	OPEN.append(s_start)
+	solution = ComputePath(OPEN, s_goal)
+	return solution
+
