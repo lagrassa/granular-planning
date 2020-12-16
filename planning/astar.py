155929b9253db87b1304afba06dd41f171f8e88a
juanluivn <45583385+juanluivn@users.noreply.github.com>
2020-12-16 02:19:47 -0500

    Update astar.py

--- planning/astar.py
+++ planning/astar.py
@@ -16,14 +16,21 @@ def ComputePath(OPEN, graph):
     free_motion_count = 0
     total_transitions = 0
     while len(OPEN) > 0:
+
+        print("\t1: Number of vertices:", len(OPEN))
+
         vertexID = heapq.heappop(OPEN)[1]
         CLOSED.add(vertexID)
 
         S = graph.getNode(vertexID)
         num_expanded +=1
         # Extract S with smallest f from OPEN
+		
+        print("VertexID is", vertexID)
+
+
         if graph.isGoal(S):    # Create function to compare two instances of class Node
-            print("Find Goal!!!")
+            print("\nFound Goal!!!")
             found_goal = True
             while S.parentId!= -1:
                 Sparent = graph.getNode(S.parentId)
@@ -38,6 +45,8 @@ def ComputePath(OPEN, graph):
             plan.reverse()
             break
 
+        print("\t2: Number of vertices:", len(OPEN))
+
         successors, fm_count, tt = graph.getSuccessors(vertexID)
         free_motion_count += fm_count
         total_transitions += tt
@@ -45,8 +54,11 @@ def ComputePath(OPEN, graph):
         if num_expanded % 200 == 0:
             print("Expanding:{}, g={}, h={}, f={}".format(S.robotState, S.g, S.h, S.f))
         # ipdb.set_trace()
+#        print("---Looking at successors to add")
         for act, s_prime in enumerate(successors):
+#            print(s_prime)
             if s_prime not in CLOSED:
+#                print("\t",s_prime)
                 SNode = graph.getNode(s_prime)
                 cost = 1.0 # TODO(???): cost from motion model?
                 if SNode.g > S.g + cost:
@@ -55,6 +67,9 @@ def ComputePath(OPEN, graph):
                     SNode.parentActionId = act
                     # print("New path", act, S.robotState, SNode.robotState)
                     heapq.heappush(OPEN, (SNode.f, s_prime))
+
+        print("\t3: Number of vertices:", len(OPEN),"\n")
+
     assert found_goal
     return plan_actions, plan, free_motion_count, total_transitions
 
